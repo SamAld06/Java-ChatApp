3a98f59f0083d66956d78b3afe51066f6e2e773a
SamAld06 <samgaldred@gmail.com>
2024-12-05 15:49:06 +0000

    completed backend code

--- chat/src/main/java/com/sam/chat/config/WebSocketEventListener.java
+++ chat/src/main/java/com/sam/chat/config/WebSocketEventListener.java
@@ -1,18 +1,37 @@
 package com.sam.chat.config;
 
-import lombok.RequireArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.steretype.Component;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.sam.chat.chat.ChatMessage;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener (
-            SessionDisconnectEvent
+            SessionDisconnectEvent event
     ){
-        
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                .type(MessageType.LEAVE)
+                .sender(username)
+                .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
