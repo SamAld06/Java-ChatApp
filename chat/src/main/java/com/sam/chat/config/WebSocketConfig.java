68fbac8910bf63851315fdfe7bda2ca805c4a217
SamAld06 <samgaldred@gmail.com>
2024-12-05 12:02:18 +0000

    initial commit

---
+++ chat/src/main/java/com/sam/chat/config/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.sam.chat;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+
+}
